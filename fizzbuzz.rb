60d04477f3a64a419ab3fb03acc7c583cd6603eb
Tymeckia Kendall <tykend@gmail.com>
2018-11-20 02:13:04 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,12 +4,13 @@
 def fizzbuzz(int)
   if (int % 5 == 0) && (int % 3 == 0) 
     "Fizzbuzz" 
+    elsif int % 3 == 0 
+    "Fizz"     
     elsif int % 5 == 0 
     "Buzz"
-    elsif int % 3 == 0 
-    "Fizz" 
   end
 end
 
 fizzbuzz(3) # => You should see a return of "Fizz"
 fizzbuzz(5) # => You should see a return of nil
+fizzbuzz (15)
